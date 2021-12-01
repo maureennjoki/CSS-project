d0e7dd7f29c718e562395985ccd9d58dedafe470
maureen njoki <92115347+maureennjoki@users.noreply.github.com>
2021-12-01 14:28:57 +0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-window.onload = ()=>{
-    const menuBtn = document.querySelector(".humbuger")
-    const mobileNav = document.querySelector(".mobile-nav")
-    menuBtn.addEventListener('click', ()=>{
-      menuBtn.classList.toggle("is-active")
-      mobileNav.classList.toggle("is-active")
-    })
-}
