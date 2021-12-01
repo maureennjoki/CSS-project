36eedef546e3b15050c23ce539deb47500bdf7eb
maureen njoki <92115347+maureennjoki@users.noreply.github.com>
2021-12-01 14:39:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let name = "maureen njoki";
+var age= 20;
+const dob="20/12/2020";
+
+let name= "maureen njoki";
+//alert ("welcome" + name);
+console.log("Age: " +age);
+console.log("Date of birth: " + dob);
+
+//DOM - DOCUMENT OBJECT MODEL
+
+const navbarToggler =document.getElementById
+("navbar-togger");
+const collapde=document.querySelector(".navbar-collapse");
+
+console.log(navbarToggler);
+
+navbarToggler.addEventListener("click",function {
+  
+} 
+ 
+)
+function toggle () {
+  collapse.style.display ="flex";
+  collapse.style.flexDirection ="column";
+}
+
